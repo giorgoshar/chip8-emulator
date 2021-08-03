271854c311ebf944472398e35e83ca0c32badfb9
giorgoshar <34591362+giorgoshar@users.noreply.github.com>
2021-08-04 00:01:32 +0300

    minor changes

--- test.asm
+++ test.asm
@@ -1,20 +1,28 @@
 .org 0x200
 jmp main
+
 main:
     load v1, 0 ; load register v1 0x0
     se   v1, 0 ; if v1 == 0 skip next instruction
-    jmp  err   ; jmp err
+    jmp  err   ; else jmp err
     
     sne  v1, 1 ; if v1 != 1 skip next instruction
-    jmp  err   ; jmp err
+    jmp  err   ; else jmp err
 
+    add v1, 1  ; add 1 to v1 register
+    sne v1, 2  ; if v1 != 2 skip next instruction
+    jmp err    ; else jmp err
+
+
+    load v1, 0
     call addition
+    se   v1, 5
+    jmp err
 
     jmp  done
 inf: jmp inf
 
 addition:
-    load v1, 0
     load v2, 5
     add  v1, v2
     ret
