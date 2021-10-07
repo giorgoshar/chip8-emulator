c4d36709ac52f97ae17d8f110cb81b5b17500ada
giorgoshar <34591362+giorgoshar@users.noreply.github.com>
2021-10-07 18:22:24 +0300

    Trying to create loops

--- test.asm
+++ test.asm
@@ -1,36 +1,50 @@
 .org 0x200
 jmp main
-.ascii "Hello World"
-.byte x 0
-.byte y 0
-.byte n 0
+.byte n 0x0000
 main:
-    load vf, 0
+    load v4, 0
     load v5, 0
+    load v0, 0
     loop:
-        cls
-        call load8
-        load v1, v6
-        call draw_number
+        call inc_n
+        ; sne v0 0xa
+        ; call draw_0
+        ; call draw_1
         jmp loop
+    
     jmp inf
 inf: jmp inf
 
-load8:
-    load [I], x
-    load [I], v1
-    load [I], 0
+draw_0:
+    cls
+    load v4, 0
+    load v5, 0
+    load [I] 0
+    draw v4, v5, 5        ; draw
+    ret
+draw_1:
+    cls
+    load v4, 0
+    load v5, 0
+    load [I] 5
+    draw v4, v5, 5        ; draw
     ret
 
-draw_number:
-    call print
+inc_n:
+    cls
+    load [I] n            ; load addr n in [I]
+    load v0 [I]           ; store values of addr [I] in regs V0...Vx
+    add  v0 5             ; add v0 5
+    load [I] n            ; load again mem addr n to [I]
+    load [I] v0           ; store V0...Vx to addr in memory starting at [I]
+    load [I] 0            ; set [I] to 0
+    add  [I] v0           ; [I] += v0
+    draw v4, v5, 5        ; draw
     ret
-print:
-   draw v4, v5, 5
-   ret 
 
-.font letter_A 0x1c 0x22 0x3e 0x22 0x22, endfont
-.font letter_E 0x7c 0x40 0x7c 0x40 0x7c, endfont
+font_start:
+.font letter_A 0x20 0x50 0x88 0xF8 0x88, endfont
+.font letter_E 0xF8 0xC0 0xF0 0xC0 0xF8, endfont
 .font letter_R 0x7c 0x44 0x7c 0x78 0x44, endfont
 .font letter_D 0x78 0x44 0x44 0x44 0x78, endfont
 .font letter_O 0x7c 0x44 0x44 0x44 0x7c, endfont
